03ded62170417a65688fa1aca173f70321740a83
doiboroi <doiboroi@users.noreply.github.com>
2022-03-09 18:47:56 +0700

    Update tf.js

--- tf.js
+++ tf.js
@@ -1,6 +1,51 @@
-function share_function(){
-	// page down
-	jQuery("body").append( '<div class="pdown"></div>' )
+jQuery("head").append('<style>\
+	.my-setting,#prev_chap, #next_chap{ opacity:0.5; position: fixed; right: 0; bottom: 0; z-index: 99999; }\
+	.my-setting,.pdown{opacity:0.5;width:110px;height:47px;position:fixed;bottom:0;left:50%;transform:translateX(-50%);background-color: #5cb85c;}\
+	.my-setting{cursor:pointer;opacity:0.3;line-height:47px;right: auto;left:0;width:110px;transform:none;text-align:center;font-size:25px;}\
+	.my-setting.active{opacity:0.7}\
+	.close-btn{z-index:999999}\
+	.cover-scroll{position: fixed;left: 0;right: 0; top: 0;bottom: 47px;display:none;}\
+	.cover-scroll.active{display:block}\
+	</style>')
+
+
+// cover scroll
+jQuery("body").append('<div class="cover-scroll"></div>')
+
+// page down
+jQuery("body").append( '<div class="pdown"></div>' )
+let iNextW = jQuery("#next_chap").outerWidth()
+let iWinW = window.innerWidth
+let iPdownW = iWinW - iNextW * 2 - 6
+jQuery(".pdown").css("width", iPdownW + "px")
+jQuery("body").on("click", ".pdown, .cover-scroll", function(){
+	let wH = window.innerHeight
+	let wSc = jQuery(window).scrollTop()
+
+	let onePage = wH + wSc - 70
+	jQuery(window).scrollTop( onePage )
+})
+
+
+//setting
+jQuery("body").append( '<div class="my-setting"><span class="glyphicon glyphicon-cog"></span></div>' )
+jQuery(".my-setting").css("width", iNextW + "px")
+jQuery("body").on("click",'.my-setting', function(){
+	jQuery(this).toggleClass("active")
+	if( jQuery(this).hasClass("active") ){
+		jQuery(".cover-scroll").css("display","block")
+	}else{
+		jQuery(".cover-scroll").css("display","none")
+	}
+
+})
+
+
+jQuery(window).resize(function(){
+	let iNextW = jQuery("#next_chap").outerWidth()
+	let iWinW = window.innerWidth
+	let iPdownW = iWinW - iNextW * 2 - 6
+	jQuery(".pdown").css("width", iPdownW + "px")
 	jQuery("body").on("click", ".pdown", function(){
 		let wH = window.innerHeight
 		let wSc = jQuery(window).scrollTop()
@@ -8,6 +53,13 @@ function share_function(){
 		let onePage = wH + wSc - 70
 		jQuery(window).scrollTop( onePage )
 	})
+
+
+	jQuery(".my-setting").css("width", iNextW + "px")
+})
+
+function share_function(){
+	
 
 	// close btn
 	jQuery("body").on("click", ".close-btn", function(){
@@ -24,7 +76,7 @@ function share_function(){
 	},100)
 
 
-	var iCheckAdCount = 1000
+	var iCheckAdCount = 50 // 10 seconds
 	var iCheckAd = setInterval(function(){
 
 		jQuery("div").each(function(){
@@ -38,7 +90,8 @@ function share_function(){
 		}
 
 	}, 200 )
-	
+
+
 }
 
 
@@ -54,22 +107,16 @@ if( window.location.href.indexOf('full.vn') != -1 ){
 		jQuery(".ads-responsive").remove()
 	}
 
+	remove_ad()
+
 	jQuery(document).ready(function(){
 		remove_ad()
 	})
-
 
 	jQuery(window).load(function(){
 		remove_ad()
 		share_function()
 	})
-
-	jQuery("head").append('<style>\
-	#prev_chap, #next_chap{ opacity:0.5; position: fixed; right: 0; bottom: 0; z-index: 99999; }\
-	#prev_chap{ right: auto;left:0 }\
-	.pdown{opacity:0.5;width:110px;height:47px;position:fixed;bottom:0;left:50%;transform:translateX(-50%);background-color: #5cb85c;}\
-	.close-btn{z-index:999999}\
-	</style>')
 }else if( window.location.href.indexOf('full.com') != -1 ){
 
 	function remove_ad(){
@@ -79,6 +126,8 @@ if( window.location.href.indexOf('full.vn') != -1 ){
 		jQuery("div[id^=fly]").remove()
 	}
 
+	remove_ad()
+
 	jQuery(document).ready(function(){
 		remove_ad()
 	})
@@ -88,14 +137,5 @@ if( window.location.href.indexOf('full.vn') != -1 ){
 		remove_ad()
 		share_function()
 	})
-
-
-	jQuery("head").append('<style>\
-	.prevChapter, .nextChapter{opacity:0.5; position: fixed; right: 0; bottom: 0; z-index: 99999; }\
-	.prevChapter{ right:auto;left:0 }\
-	.pdown{opacity:0.5;width:110px;height:47px;position:fixed;bottom:0;left:50%;transform:translateX(-50%);background-color: #5cb85c;}\
-	.close-btn{z-index:999999}\
-	</style>')
-	
 }
 
